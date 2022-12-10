83952e9d4c57c5c174ffc67c2e83663ae66a382b
vishnura-Kore <92306687+vishnura-Kore@users.noreply.github.com>
2022-12-10 22:15:56 +0530

    ch

--- src/test/java/TestRunner3.java
+++ src/test/java/TestRunner3.java
@@ -3,7 +3,7 @@ import org.junit.runner.RunWith;
 
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
-@KarateOptions(features = { "classpath:token","classpath:CustomerValidationBot1","classpath:CustomerValidationBot2","classpath:publicAPIs"})
+@KarateOptions(features = { "classpath:token","classpath:CustomerValidationBot1"})
 @RunWith(Karate.class)
 public class TestRunner3
 {
