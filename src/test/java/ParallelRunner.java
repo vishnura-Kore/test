69a8dfac977827a3920fc0e3902ff8f5ef8f8035
vishnura-Kore <92306687+vishnura-Kore@users.noreply.github.com>
2022-12-10 17:10:43 +0530

    ch

--- src/test/java/ParallelRunner.java
+++ src/test/java/ParallelRunner.java
@@ -24,7 +24,7 @@ public class ParallelRunner {
 	public void executeKarateTests() {
 		Runner.parallel(getClass(), 15);
 		Builder aRunner = new Builder();
-		aRunner.path("classpath:CustomerValidationBot1");
+		aRunner.path("classpath:token");
 		aRunner.outputCucumberJson(true);
 		aRunner.parallel(15);
 		//Runner.parallel(aRunner);
