44acb41c15dbed66c0e97275d8fb4a845aac3864
vishnura-Kore <92306687+vishnura-Kore@users.noreply.github.com>
2022-12-10 17:34:09 +0530

    change

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -9,66 +9,66 @@ env = 'preprod';
 
 var config = {
 
-                 appUrl: 'https://preprod-bots.korebots.com/api/1.1/', 
+                 appUrl: 'https://staging-bots.korebots.com/api/1.1/', 
                 username: 'botowner@koreai.in', 
                 password: 'Kore#123',
                 jwturl: 'https://demo.kore.net/mock/util/sts', 
-                publicUrl: 'https://preprod-bots.korebots.com/api', 
-                runtimeUrl: 'https://preprod-bots.korebots.com',
+                publicUrl: 'https://staging-bots.korebots.com/api', 
+                runtimeUrl: 'https://staging-bots.korebots.com',
                 }
 
 if(env == 'staging') {
-                config.appUrl = 'https://staging-bots.korebots.com/api/1.1/',
-                config.username = 'botowner@koreai.in',
-                config.password = 'Kore#123' ,
-                config.jwturl = 'https://demo.kore.net/mock/util/sts',
-                config.publicUrl ='https://staging-bots.korebots.com/api',
-                config.runtimeUrl= 'https://staging-bots.korebots.com',
+                config.appUrl = 'https://staging-bots.korebots.com/api/1.1/';
+                config.username = 'botowner@koreai.in';
+                config.password = 'Kore#123' ;
+                config.jwturl = 'https://demo.kore.net/mock/util/sts';
+                config.publicUrl ='https://staging-bots.korebots.com/api';
+                config.runtimeUrl= 'https://staging-bots.korebots.com';
 }
 else if(env == 'qa1') {
            config.appUrl = 'https://qa1-bots.kore.ai/api/1.1/',
            config.username = 'owner@koreai.in',
-           config.password = 'Kore@123',
+           config.password = 'Kore@123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://qa1-bots.kore.ai/api/',
-           config.runtimeUrl= 'https://qa1-bots.kore.ai',
+           config.runtimeUrl= 'https://qa1-bots.kore.ai'
 }
  
 else if(env == 'prod'){
            config.appUrl = 'https://bots.kore.ai/api/1.1/',
            config.username = 'botowner@koreai.in',
-           config.password = 'Kore#123',
+           config.password = 'Kore#123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://bots.kore.ai/api/',
-           config.runtimeUrl = 'https://bots.kore.ai',
+           config.runtimeUrl = 'https://bots.kore.ai'
            }
            
            
            else if(env == 'preprod'){
-           config.appUrl = 'https://preprod-bots.korebots.com/api/1.1/',
-           config.username = 'botowner@koreai.in',
-           config.password = 'Kore#123',
-           config.jwturl = 'https://demo.kore.net/mock/util/sts',
-           config.publicUrl = 'https://preprod-bots.korebots.com/api',
-           config.runtimeUrl = 'https://preprod-bots.korebots.com',
+           config.appUrl = 'https://preprod-bots.korebots.com/api/1.1/';
+           config.username = 'botowner@koreai.in';
+           config.password = 'Kore#123';
+           config.jwturl = 'https://demo.kore.net/mock/util/sts';
+           config.publicUrl = 'https://preprod-bots.korebots.com/api';
+           config.runtimeUrl = 'https://preprod-bots.korebots.com';
            }
            
            
            else if(env == 'JAPAN') {
            config.appUrl = 'https://jp-bots.kore.ai/api/1.1',
            config.username = 'owner@koreai.in',
-           config.password = 'Kore#123',
+           config.password = 'Kore#123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://jp-bots.kore.ai/api/',
-           config.runtimeUrl= 'https://jp-bots.kore.ai',
+           config.runtimeUrl= 'https://jp-bots.kore.ai'
 }
   else if(env == 'EUPROD') {
            config.appUrl = 'https://eu-bots.kore.ai/api/1.1',
            config.username = 'owner@koreai.in',
-           config.password = 'Kore@123456',
+           config.password = 'Kore@123456'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://eu-bots.kore.ai/api/',
-           config.runtimeUrl= 'https://eu-bots.kore.ai',
+           config.runtimeUrl= 'https://eu-bots.kore.ai'
 }
 
 else if(env == 'post') {
@@ -77,30 +77,30 @@ else if(env == 'post') {
            config.password = 'Kore@123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://ebaysandbox.korebots.com/api/',
-           config.runtimeUrl= 'https://ebaysandbox.korebots.com',
+           config.runtimeUrl= 'https://ebaysandbox.korebots.com'
 }
 
 else if(env == 'sit') {
            config.appUrl = 'https://sit-bots.kore.ai/api/1.1',
            config.username = 'owner@koreai.in',
-           config.password = 'Kore@123',
+           config.password = 'Kore@123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://sit-bots.kore.ai/api/',
-           config.runtimeUrl= 'https://sit-bots.kore.ai',
+           config.runtimeUrl= 'https://sit-bots.kore.ai'
 }
 
 else if(env == 'IDFC') {
            config.appUrl = 'https://bots-dev.korebots.com/api/1.1',
            config.username = 'jan12@idfc.com',
-           config.password = 'IdfcKore@123',
+           config.password = 'IdfcKore@123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://bots-dev.korebots.com/api/',
-           config.runtimeUrl= 'https://bots-dev.korebots.com',
+           config.runtimeUrl= 'https://bots-dev.korebots.com'
 }
 else if(env == 'DE-BOTS') {
            config.appUrl = 'https://de-bots.kore.ai/api/1.1',
            config.username = 'owner@koreai.in',
-           config.password = 'Kore#123',
+           config.password = 'Kore#123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://de-bots.kore.ai/api/',
            config.runtimeUrl= 'https://de-bots.kore.ai'
@@ -108,7 +108,7 @@ else if(env == 'DE-BOTS') {
 else if(env == 'installer') {
            config.appUrl = 'https://installer-598-use1.korebots.com/api/1.1',
            config.username = 'installer@kore.com',
-           config.password = 'Kore@123',
+           config.password = 'Kore@123'
            config.jwturl = 'https://demo.kore.net/mock/util/sts',
            config.publicUrl = 'https://installer-598-use1.korebots.com/api/',
            config.runtimeUrl= 'https://installer-598-use1.korebots.com'
